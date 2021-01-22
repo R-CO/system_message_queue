eb7a679f2b5cd37a9473644931da7d3f012682b9
R-CO <daniel1820kobe@gmail.com>
2021-01-22 21:31:02 +0800

    [feat] Test case implement
    1. Implement case "bindQueueViaKey".
    2. Refact case "createQueue".

--- test/system_message_queue_test.cpp
+++ test/system_message_queue_test.cpp
@@ -36,15 +36,24 @@ class SystemMessageQueueTest : public ::testing::Test {
 
 TEST_F(SystemMessageQueueTest, createQueue) {
   for (size_t i = 0; i < kKeyCount_; ++i) {
-    EXPECT_TRUE(smq_vector_[i]->createQueue(key_vector_[i]));
-    EXPECT_TRUE(smq_vector_[i]->removeQueue());
+    auto &smq = *smq_vector_[i];
+    EXPECT_TRUE(smq.createQueue(key_vector_[i]));
+    EXPECT_NE(smq.getQueueId(), -1);
+    EXPECT_TRUE(smq.removeQueue());
   }
 }
 
 TEST_F(SystemMessageQueueTest, bindQueueViaKey) {
   for (auto key : key_vector_) {
-    rco::SystemMessageQueue smq;
-    EXPECT_TRUE(smq.bindQueueViaKey(key));
+    rco::SystemMessageQueue smq1;
+    EXPECT_TRUE(smq1.createQueue(key));
+    EXPECT_NE(smq1.getQueueId(), -1);
+
+    rco::SystemMessageQueue smq2;
+    EXPECT_TRUE(smq2.bindQueueViaKey(key));
+    EXPECT_NE(smq2.getQueueId(), -1);
+    EXPECT_EQ(smq1.getQueueId(), smq1.getQueueId());
+    EXPECT_TRUE(smq2.removeQueue());
   }
 }
 
