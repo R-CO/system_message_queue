43c0ec061f0b8f215e871b5cd5c74d33868ce46b
danielsw.chang <danielsw.chang@yuanta.com>
2021-01-28 17:35:28 +0800

    [feat] Add template version of sendToQueue()
    1. Add template version of sendToQueue().
    2. Define 'DEBUG_RCO' to measure time span of sysytem call msgsnd() and msgrcv().
    3. Add new test case for template version of sendToQueue().
    
    Change-Id: I41da857e866034d34c6fe426625200d4aca449d0

--- system_message_queue.hpp
+++ system_message_queue.hpp
@@ -6,11 +6,38 @@
 *** Date: 2021-01-22
 **/
 // unix system header
+#include <errno.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/types.h>
 
 namespace rco {
+
+namespace sysmsgq {
+
+/*
+ * To inherit this structure for calling template function send/receive
+ * ex:
+ *   struct MyData : public rco::sysmsgq::MessageBufferBase {
+ *     struct TYPE_OF_CONSEQUENT_MEMORY_BLOCK {
+ *       // ...
+ *     };
+ *   };
+ *   rco::SystemMessageQueue smq;
+ *   smq.createQueueViaKey(key);
+ *   MyData data;
+ *   data.mtype = 1;
+ *   // data.actual_data = XXXXXX;
+ *   smq.sendToQueue(data, msgflg);
+ */
+struct MessageBufferBase {
+  long mtype;
+
+  MessageBufferBase() : mtype(0) {}
+  MessageBufferBase(const long message_type) : mtype(message_type) {}
+};
+
+}  // end of namespace sysmsgq
 
 class SystemMessageQueue {
  public:
@@ -48,8 +75,8 @@ class SystemMessageQueue {
   /*
    * structure should be somthing like
    * struct {
-   *   long mtype; // It must have, and named it "mtype".
-   *   char mtext[1] // message data
+   *   long mtype; // Structure must have this member(1st), and it's type must
+   * be 'long int'. char mtext[1] // message data
    * }
    *
    * Please refer to `int msgsnd(int msqid, const void *msgp, size_t msgsz, int
@@ -58,6 +85,20 @@ class SystemMessageQueue {
   bool sendToQueue(const void* msgp, const size_t msgsz, const int msgflg);
   ssize_t receiveFromQueue(void* msgp, const size_t msgsz, const long msgtyp,
                            const int msgflg);
+
+  template <typename T>
+  bool sendToQueue(const T& msgst, const int msgflg) {
+    const sysmsgq::MessageBufferBase& msg_buffer = msgst;
+    enum ReturnCode { ok = 0, fail = -1 };
+    if (msgsnd(this->getQueueId(), &msgst,
+               sizeof(msgst) - sizeof(msg_buffer.mtype),
+               msgflg) == static_cast<int>(ReturnCode::fail)) {
+      this->error_no_ = errno;
+      return false;
+    }
+
+    return true;
+  }
 
   int getQueueId() const;
   int getErrorNo() const;
@@ -76,4 +117,4 @@ class SystemMessageQueue {
 
 }  // end of namespace rco
 
-#endif  // end of defin RCO_UTILITY_SYSTEM_MESSAGE_QUEUE_HPP
+#endif  // end of define RCO_UTILITY_SYSTEM_MESSAGE_QUEUE_HPP
--- test/system_message_queue_test.cpp
+++ test/system_message_queue_test.cpp
@@ -10,6 +10,8 @@ using std::vector;
 
 #include <cstdio>
 
+static size_t gKeyIndex = 1;
+
 class SystemMessageQueueTest : public ::testing::Test {
  public:
   SystemMessageQueueTest()
@@ -28,13 +30,10 @@ class SystemMessageQueueTest : public ::testing::Test {
   }
   void TearDown() override {}
 
-  static size_t gKeyIndex;
   const size_t kMaxKeyCount_;
   vector<std::shared_ptr<rco::SystemMessageQueue>> smq_vector_;
   vector<key_t> key_vector_;
 };
-
-size_t SystemMessageQueueTest::gKeyIndex = 1;
 
 TEST_F(SystemMessageQueueTest, createQueue) {
   for (size_t i = 0; i < kMaxKeyCount_; ++i) {
@@ -114,7 +113,7 @@ TEST_F(SystemMessageQueueTest, sendToQueue) {
     char mbuffer[kBufferSize];
   };
 
-  string msg =
+  const string msg =
       "hello, here is a test string with long long long long long long long "
       "length.";
   auto msg_size = msg.size() + 1 /* extra 1 byte for '\0' */;
@@ -152,7 +151,7 @@ TEST_F(SystemMessageQueueTest, ReceiveFromQueue) {
     char mbuffer[kBufferSize];
   };
 
-  string msg =
+  const string msg =
       "hello, here is a test string with long long long long long long long "
       "length.";
   auto msg_size = msg.size() + 1 /* extra 1 byte for '\0' */;
@@ -171,6 +170,41 @@ TEST_F(SystemMessageQueueTest, ReceiveFromQueue) {
     EXPECT_EQ(smq.receiveFromQueue(&receive_data, sizeof(receive_data.mbuffer),
                                    MessageType::kReadFirst, kBlockMode),
               msg_size);
+  }
+
+  EXPECT_TRUE(smq.removeQueue());
+}
+
+TEST(SystemMessageQueueTest_template_func, sendToQueue) {
+  rco::SystemMessageQueue smq;
+  EXPECT_TRUE(smq.createQueue(gKeyIndex++));
+
+  const string msg =
+      "hello, here is a test string with long long long long long long long "
+      "length.";
+
+  static constexpr size_t kBufferSize = 256;
+  struct MyData : public rco::sysmsgq::MessageBufferBase {
+    MyData(const long mtype) : MessageBufferBase(mtype), mbuffer("") {}
+
+    char mbuffer[kBufferSize];
+  };
+
+  MyData send_data(1);
+  memcpy(send_data.mbuffer, msg.c_str(), msg.size() + 1);
+
+  enum MessageFlag { kBlockMode = 0 };
+  const auto is_send_success =
+      smq.sendToQueue(send_data, static_cast<int>(kBlockMode));
+  EXPECT_TRUE(is_send_success);
+
+  if (is_send_success) {
+    enum MessageType { kReadFirst = 0 };
+    MyData receive_data(kReadFirst);
+    EXPECT_EQ(
+        msgrcv(smq.getQueueId(), &receive_data, sizeof(receive_data.mbuffer),
+               MessageType::kReadFirst, kBlockMode),
+        sizeof(receive_data.mbuffer));
   }
 
   EXPECT_TRUE(smq.removeQueue());
